b1d67d4763cb36a5177d96a68901f0462ab21492
vuzcategui <59197076+vuzcategui@users.noreply.github.com>
2020-01-31 01:05:24 -0400

    MintMe Coin Wallet v1.2.3

--- electron/main.js
+++ electron/main.js
@@ -38,7 +38,7 @@ startProtocolHandler();
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-  log.info('Starting Webchain', app.getVersion());
+  log.info('Starting MintMe Coin', 'v'+app.getVersion());
   const browserWindow = mainWindow.createWindow(isDev);
   const services = new Services(browserWindow.webContents);
   ipc({ settings, services });
