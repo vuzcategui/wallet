fc9b99134b1d217db64d93192d78b710b1abd46a
vuzcategui <59197076+vuzcategui@users.noreply.github.com>
2020-01-31 00:28:03 -0400

    MintMe Coin Wallet v1.2.3 - Replaced WEB to MINTME

--- src/components/welcome/Terms/Terms.js
+++ src/components/welcome/Terms/Terms.js
@@ -15,7 +15,7 @@ export const Terms = ({ onAgree }) => {
       <Grid item xs={12}>
         <div style={style}>
           <p>
-            End-User License Agreement for Webchain Wallet
+            End-User License Agreement for MintMe Coin Wallet
           </p>
           <p>
             This End-User License Agreement ("EULA") is a legal agreement between you (either an individual or a single entity) and authors, including all contributors to the source code, ("AUTHORS") of this Software identified above, which includes computer software and may include associated media, printed materials, and “online” or electronic documentation (“SOFTWARE”).
@@ -27,7 +27,7 @@ export const Terms = ({ onAgree }) => {
             THE SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, AND NO WARRANTY, EITHER EXPRESSED OR IMPLIED, IS GIVEN. YOUR USE OF THE SOFTWARE IS AT YOUR SOLE RISK. AUTHORS do not warrant that (i) the SOFTWARE will meet your specific requirements; (ii) the SOFTWARE is fully compatible with any particular platform; (iii) your use of the SOFTWARE will be uninterrupted, timely, secure, or error-free; (iv) the results that may be obtained from the use of the SOFTWARE will be accurate and/or reliable; (v) the quality of any products, services, information, or other material purchased or obtained by you through the SOFTWARE will meet your expectations; or (vi) any errors in the SOFTWARE will be corrected.
           </p>
           <p>
-            YOU EXPRESSLY UNDERSTAND AND AGREE THAT AUTHORS SHALL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL OR EXEMPLARY DAMAGES, INCLUDING BUT NOT LIMITED TO, DAMAGES FOR LOSS OF PROFITS, GOODWILL, USE, DATA OR OTHER INTANGIBLE LOSSES (EVEN IF AUTHORS OR WEBCHAIN NETWORK MEMBERS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES) RELATED TO THE SOFTWARE, including, for example: (i) the use or the inability to use the SOFTWARE; (ii) the cost of procurement of substitute goods and services resulting from any goods, data, information or services purchased or obtained or messages received or transactions entered into, through or from the SOFTWARE; (iii) unauthorized access to or alteration of your transmissions or data; (iv) statements or conduct of any third-party on the SOFTWARE; (v) or any other matter related to the SOFTWARE.
+            YOU EXPRESSLY UNDERSTAND AND AGREE THAT AUTHORS SHALL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL OR EXEMPLARY DAMAGES, INCLUDING BUT NOT LIMITED TO, DAMAGES FOR LOSS OF PROFITS, GOODWILL, USE, DATA OR OTHER INTANGIBLE LOSSES (EVEN IF AUTHORS OR MINTME.COM COIN MEMBERS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES) RELATED TO THE SOFTWARE, including, for example: (i) the use or the inability to use the SOFTWARE; (ii) the cost of procurement of substitute goods and services resulting from any goods, data, information or services purchased or obtained or messages received or transactions entered into, through or from the SOFTWARE; (iii) unauthorized access to or alteration of your transmissions or data; (iv) statements or conduct of any third-party on the SOFTWARE; (v) or any other matter related to the SOFTWARE.
           </p>
         </div>
       </Grid>
